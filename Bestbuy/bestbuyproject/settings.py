1ac7a387a33ca8b60a2ff3f9763ceb9bba0ce23f
Ajay KJ <102035919+Ajaykj684@users.noreply.github.com>
2022-07-30 23:52:39 +0530

    Update settings.py

--- Bestbuy/bestbuyproject/settings.py
+++ Bestbuy/bestbuyproject/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g1b%e86rswtcx19q5qipxja)k(x=mu6qd15p$*79c!d2#-n_w&'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
@@ -153,6 +154,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 RAZORPAY_KEY_ID = config('RAZORPAY_KEY_ID')
 RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET')
 ACCOUNT_SECURITY_API_KEY = config('ACCOUNT_SECURITY_API_KEY')
+
+
+
 
 
 
