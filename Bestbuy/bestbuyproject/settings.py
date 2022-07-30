3c374183aa124a614ca46de13849e29a8901c5de
Ajay KJ <102035919+Ajaykj684@users.noreply.github.com>
2022-07-30 23:55:40 +0530

    Update settings.py

--- Bestbuy/bestbuyproject/settings.py
+++ Bestbuy/bestbuyproject/settings.py
@@ -96,10 +96,16 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME'  : 'mydatabase',
+         'USER'  : 'postgres',
+         'PASSWORD' : '12345',
+         'HOST' : 'localhost',
+         'PORT' : '5432',
+
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
